df116db782daf609b3f0435feb10f364dc7a29e9
Juan Marcano <juancrlsmarcano@gmail.com>
2024-08-08 11:12:30 -0500

    entrega el texto de descrición con simbolo '\n' salto de linea, se puede eliminar desde excel. No entrega categoría. Debe editarse en el archivo de excel. Mejoras: mensaje de error cuando el archivo de excel esta abierto, abrir archi de excel al final para visualizar.

--- abrir_CSV_PRODUCTOS_WORDPRESS.py
+++ abrir_CSV_PRODUCTOS_WORDPRESS.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
+import re
+from bs4 import BeautifulSoup
 
 def cargar_csv():
     # Crear una ventana oculta de Tkinter
@@ -65,55 +67,79 @@ def crear_dataframe_imagenes(df_csv):
     df_separado = df_csv['Imágenes'].str.split(', ', expand=True)
     df_separado.columns = ['Imagen1','Imagen2','Imagen3','Imagen4','Imagen5','Imagen6','Imagen7','Imagen8','Imagen9','Imagen10','Imagen11','Imagen12','imagen13']
     print (df_separado)
-    return df_separado   
+    return df_separado 
+  
+def convertir_html_a_texto(texto_html):
+    if isinstance(texto_html, str):
+        # Crear un objeto BeautifulSoup solo si es un string
+        soup = BeautifulSoup(texto_html, "html.parser")
+        # Convertir el HTML a texto plano, manteniendo los saltos de línea
+        texto_plano = soup.get_text()
+        return texto_plano
+    else:
+        # Si no es un string, devolver el valor tal como está
+        return texto_html
 
+def editar_df_excel(ajustar_tamano_dataframe, df_csv, df_imagenes, df_excel):
+    if df_excel is not None:
+        df_excel=ajustar_tamano_dataframe(df_csv, df_excel)
+    # Rellenar toda la columna 'columna2' con el texto 'valor_fijo'
+        df_excel['Nombre #39'] = df_csv['Nombre']
+        df_excel['Marca #26'] = "GENERICO"
+        df_excel['Descripción #53'] = df_csv['Descripción']
+        df_excel['SKU del vendedor #29'] = df_csv['SKU']
+
+        #df_excel['Categoría primaria #1'] ='2779 - Electrónica / Computación / Componentes informáticos / Otros componentes informáticos'
+
+        #df_excel[ 'Código de barras #56'] = 
     
-# Cargar el archivo CSV
+        df_excel['Variación #1700'] = '...'
+        df_excel['TaxPercentage #2725'] = 0
+
+        df_excel['Ancho del paquete #60']=10
+        df_excel['Largo del paquete #33']=10
+        df_excel['Alto del paquete #47']=10
+        df_excel['Peso del paquete #8']=1
+
+        df_excel['PriceFalabella #52']=df_csv['Precio normal']
+        df_excel['SalePriceFalabella #18']=""
+
+        df_excel['QuantityFalabella #25']= 1
+
+        df_excel['Dimensiones #1619']= "10 cm x 10 cm x 10 cm"
+        df_excel['Condición del Producto #22']= "Nuevo"
+
+        df_excel['Imagen principal #IM1']= df_imagenes['Imagen1']
+        df_excel['Imagen2 #IM2']= df_imagenes['Imagen2']
+        df_excel['Imagen3 #IM3']= df_imagenes['Imagen3']
+        df_excel['Imagen4 #IM4']= df_imagenes['Imagen4']
+        df_excel['Imagen5 #IM5']= df_imagenes['Imagen5']
+        df_excel['Imagen6 #IM6']= df_imagenes['Imagen6']
+        df_excel['Imagen7 #IM7']= df_imagenes['Imagen7']
+        df_excel['Imagen8 #IM8']= df_imagenes['Imagen8']
+    return df_excel
+
+
+
+# Cargar el archivo CSV caja de dialogo Tkinter
 df_csv = cargar_csv()
 
-# Cargar el dataframe de imágenes
+# Aplicar la función convertir HTML a TEXTO a la columna 'descripcion'
+df_csv['Descripción'] = df_csv['Descripción'].apply(convertir_html_a_texto)
+
+# Cargamos un dataframe con las direcciones de las las imágenes 
 df_imagenes = crear_dataframe_imagenes(df_csv)
 
-# Cargar el archivo Excel
+# Cargar el archivo Excel a partir de la tercera fila que son los datos útiles 
 df_excel, excel_path = cargar_excel(skip_rows=3)
 
+# Llamamos a la funcion que va a editar el contenido dataframe
+df_excel = editar_df_excel(ajustar_tamano_dataframe, df_csv, df_imagenes, df_excel)
 
-if df_excel is not None:
-    df_excel=ajustar_tamano_dataframe(df_csv, df_excel)
-    # Rellenar toda la columna 'columna2' con el texto 'valor_fijo'
-    df_excel['Nombre #39'] = df_csv['Nombre']
-    df_excel['Marca #26'] = "GENERICO"
-    df_excel['Descripción #53'] = df_csv['Descripción']
-    #df_excel['Categoría primaria #1'] =
-    #df_excel[ 'Código de barras #56'] = 
-    df_excel['Variación #1700'] = '...'
-    df_excel['TaxPercentage #2725'] = 0
-
-    df_excel['Ancho del paquete #60']=10
-    df_excel['Largo del paquete #33']=10
-    df_excel['Alto del paquete #47']=10
-    df_excel['Peso del paquete #8']=1
-
-    df_excel['PriceFalabella #52']=df_csv['Precio normal']
-    df_excel['SalePriceFalabella #18']=""
-
-    df_excel['QuantityFalabella #25']= 1
-
-    df_excel['Dimensiones #1619']= "10 cm x 10 cm x 10 cm"
-    df_excel['Condición del Producto #22']= "Nuevo"
-
-    df_excel['Imagen principal #IM1']= df_imagenes['Imagen1']
-    df_excel['Imagen2 #IM2']= df_imagenes['Imagen2']
-    df_excel['Imagen3 #IM3']= df_imagenes['Imagen3']
-    df_excel['Imagen4 #IM4']= df_imagenes['Imagen4']
-    df_excel['Imagen5 #IM5']= df_imagenes['Imagen5']
-    df_excel['Imagen6 #IM6']= df_imagenes['Imagen6']
-    df_excel['Imagen7 #IM7']= df_imagenes['Imagen7']
-    df_excel['Imagen8 #IM8']= df_imagenes['Imagen8']
+# Escribimos el archivo de excel con los datos del dataframe
     
-
-with pd.ExcelWriter(excel_path, engine='openpyxl', mode='a') as writer:
-        df_excel.to_excel(writer, index=False, startrow=5, sheet_name="Subir plantilla",header=False)
+with pd.ExcelWriter(excel_path, engine='openpyxl', mode='a',if_sheet_exists='overlay') as writer:
+        df_excel.to_excel(writer, index=False, startrow=4, sheet_name="Subir plantilla",header=False)
 print("Archivo Excel modificado y guardado correctamente.")
 
     
